bc971fa8c434441787098b4f61faf376bcebdc41
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-07-15 12:18:47 -0300

    chore: init project

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const app = require("./app");
+
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("Conectado a MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Servidor corriendo en puerto ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error conectando a MongoDB:", err);
+  });
